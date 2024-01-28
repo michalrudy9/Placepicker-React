262115fa343bb7bf523cfcaab7600e785757cb3f
michalrudy9 <michallrudy9@gmail.com>
2024-01-28 14:23:12 +0100

    Transformed response data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Places from "./Places";
 import Error from "./Error";
+import { sortPlacesByDistance } from "../loc";
 
 function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvaliablePlaces] = useState([]);
@@ -11,19 +12,27 @@ function AvailablePlaces({ onSelectPlace }) {
     setIsFetching(true);
     async function fetchPlaces() {
       try {
-        const response = await fetch("http://localhost:3000/pplaces");
+        const response = await fetch("http://localhost:3000/places");
         const resData = await response.json();
         if (!response.ok) {
           throw new Error("Failed to fetch places!");
         }
-        setAvaliablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvaliablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || "Could not fetch places, please try again later.",
         });
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
