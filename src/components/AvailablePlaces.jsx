e7ae7f5e6cd740795f2eeb832f3d1b628788467c
michalrudy9 <michallrudy9@gmail.com>
2024-01-28 13:19:16 +0100

    Added fetching alert

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,12 +3,15 @@ import Places from "./Places";
 
 function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvaliablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
+    setIsFetching(true);
     async function fetchPlaces() {
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvaliablePlaces(resData.places);
+      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -16,8 +19,10 @@ function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      loadingText="Fetching place data ..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isFetching}
     />
   );
 }
